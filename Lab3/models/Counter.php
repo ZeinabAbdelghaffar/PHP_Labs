9aeaa7711cac977e2ddb6acaa10937c99a17ff91
Zeinab Abdelghaffar <zeinanabdo88@users.noreply.github.com>
2024-03-06 11:59:42 +0200

    Update Counter.php

--- Lab3/models/Counter.php
+++ Lab3/models/Counter.php
@@ -1,10 +1,8 @@
 <?php
-
 class Counter
 {
     public static function getCount()
     {
-
         $visits = file_get_contents("visits.txt");
         return intval($visits);
     }
@@ -13,29 +11,8 @@ class Counter
     {
         $new_count = self::getCount() + 1;
         $fp = fopen("visits.txt", "w");
-        fwrite($fp, $new_count);
+        fwrite($fp, strval($new_count)); 
         fclose($fp);
     }
 }
 
-<?php
-class Counter {
-    private $filePath;
-    public function __construct($filePath) {
-        $this->filePath = $filePath;
-    }
-    public function incrementCounter() {
-        $count = $this->getCount();
-        $count++;
-        file_put_contents($this->filePath, $count);
-        return $count;
-    }
-    public function getCount() {
-        if (file_exists($this->filePath)) {
-            return intval(file_get_contents($this->filePath));
-        } else {
-            return 0;
-        }
-    }
-}
-?>
